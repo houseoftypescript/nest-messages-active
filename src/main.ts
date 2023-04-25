e65fb8dab8d7c4504eea005d71c6627cff2af093
hieudoanm <hieumdoan@gmail.com>
2023-04-25 12:02:09 +0700

    update postman

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { writeFileSync } from 'fs';
+import helmet from 'helmet';
+import { dump } from 'js-yaml';
 import { AppModule } from './app.module';
+import environments from './common/environments/environments';
 
-async function bootstrap() {
+const swaggerify = (app: INestApplication) => {
+  const config = new DocumentBuilder()
+    .setTitle('Nest.js - ActiveMQ - Template')
+    .setDescription('Nest.js - ActiveMQ - Template')
+    .setVersion('1.0')
+    .addTag('Nest.js')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  const swaggerPath = './docs/swagger';
+  const json = JSON.stringify(document, null, 2);
+  writeFileSync(`${swaggerPath}/swagger.json`, json);
+  const yaml: string = dump(document);
+  writeFileSync(`${swaggerPath}/swagger.yaml`, yaml);
+  return document;
+};
+
+const bootstrap = async () => {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-}
+  app.enableCors();
+  app.use(helmet());
+  if (environments.environment === 'development') swaggerify(app);
+  await app.listen(environments.port);
+};
+
 bootstrap();
